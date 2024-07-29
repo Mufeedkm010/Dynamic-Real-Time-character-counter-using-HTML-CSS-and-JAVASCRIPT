4298e2c6637da8085f3d488bdef8da43ea8045aa
Mufeedkm010 <166125773+Mufeedkm010@users.noreply.github.com>
2024-07-29 16:42:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('inputText');
+    const typedCount = document.getElementById('typedCount');
+    const remainingCount = document.getElementById('remainingCount');
+
+    textarea.addEventListener('input', () => {
+        const typed = textarea.value.length;
+        const remaining = 50 - typed;
+        typedCount.textContent = `${typed} characters typed`;
+        remainingCount.textContent = `${remaining} characters remaining`;
+    });
+});
